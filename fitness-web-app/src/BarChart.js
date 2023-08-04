785b1e1127af0c304a1e688e01d710f74e1d0ca0
unknown <drobinson51@qub.ac.uk>
2023-08-04 20:33:56 +0100

    Changed up barchart, gets average weekly data and then outputs it

--- fitness-web-app/src/BarChart.js
+++ fitness-web-app/src/BarChart.js
@@ -32,7 +32,18 @@ const UserBarChart = () => {
           `http://localhost:4000/userbarchart/${cookies.authUser}`
         );
         const userData = response.data.data;
-        setUserBarChartData(userData);
+
+
+          //date time is converted into JS date object
+        const dataDividedByDate = userData.map((item) => ({
+          ...item,
+          timestamp: new Date(item.timestamp)
+        }));
+
+        //sorts the data timestamps in ascending order
+        dataDividedByDate.sort((a, b) => a.timestamp - b.timestamp);
+
+        setUserBarChartData(dataDividedByDate);
 
         const uniqueExercises = [...new Set(userData.map((item) => item.workoutname))];
 
@@ -55,19 +66,75 @@ const UserBarChart = () => {
   };
 
   //filters the userData to only return the exercise data that matches what is found in the option map
-  const filteredData = userChartData.filter(
-    (dataset) => dataset.workoutname === selectedExercise
-  );
+  // const filteredData = userChartData.filter(
+  //   (dataset) => dataset.workoutname === selectedExercise
+  // );
+
+  const getWeeklyAverages = () => {
+    const weeklyAverages = [];
+    //curent week keeps the averages contained to that particular week, once it encounters a different week it moves on
+    let currentWeek = null;
+    let sumWeights = 0;
+    let countWeights = 0;
+
+    for (const item of userChartData) {
+      //sets the format of week which is identical to the format of currentWeek so they are compared
+      const week = item.timestamp.toLocaleDateString("en-UK", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+
+
+      //if selected exercise isn't picked, then the array will not be formed of the week from the timestamp and average
+      if (item.workoutname === selectedExercise) {
+        //if the week isn't current week then that means it has moved on in which case the previous weeks data can be pushed to the array
+      if (week !== currentWeek) {
+        
+
+        if (currentWeek !== null) {
+          weeklyAverages.push({
+            week: currentWeek,
+            averageWeight: sumWeights / countWeights,
+          });
+        }
+
+        //sets default values again if week doesn't equal current week
+        currentWeek = week;
+        sumWeights = 0;
+        countWeights = 0;
+      }
+
+      //sum of weights is pushed every time week
+      sumWeights += item.totalweightlifted
+      countWeights++;
+
+
+    }
+  }
+
+  //manually have to push last value, as loop condition terminates and there is no next week
+    if (currentWeek !== null) {
+      weeklyAverages.push({
+        week: currentWeek,
+        averageWeight: sumWeights / countWeights,
+      });
+    }
+    return weeklyAverages;
+  }
+
+
+  const weeklyAverages = getWeeklyAverages();
 
   //filtered data used allowing data to swap based on what exercise has been selected.
-  const labels = filteredData.map((item) => item.workoutname);
-  const values = filteredData.map((item) => item.totalweightlifted);
+  const labels = weeklyAverages.map((item) => item.week);
+  const values = weeklyAverages.map((item) => item.averageWeight);
 
   const chartData = {
     labels: labels,
     datasets: [
       {
-        label: "Weight Lifted",
+        label: "Weight Average Weight Lifted",
         data: values,
         backgroundColor: "rgba(75, 192, 192, 0.6)",
         borderColor: "rgba(75, 192, 192, 1)",
